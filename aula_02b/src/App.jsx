74e2944041d9e4b22f7ebfcaab6a6ae2cb0af0bf
Luiz Carlos Santana <112254950+luizcarlossantana@users.noreply.github.com>
2024-03-20 18:17:49 -0300

    ok

---
+++ aula_02b/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import RenderizandoComFuncoes from './components/RenderizandoComFuncoes'
+import TrabalhadoComImagens from './components/TrabalhadoComImagens'
+import HookContador from './components/HookContador'
+import HookMegaSena from './components/HookMegaSena'
+
+const App = () => {
+  return (
+    <div>
+      <RenderizandoComFuncoes />
+      <TrabalhadoComImagens />
+      <HookContador />
+      <HookMegaSena />
+    </div>
+  )
+}
+
+export default App
